830ccfffe3a3df52b6d58f7c299ed70de3fc53be
houtaib <houtayb@gmail.com>
2025-02-24 07:49:38 +0000

    toglle the nab in the phone

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-// Menu
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu');
+};
+
+// Close menu on page scroll
+window.onscroll = () => {
+    menu.classList.remove('move');
+    navbar.classList.remove('open-menu');
 };
